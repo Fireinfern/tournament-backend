9490dd2610eb3253479126a952fa0a7347765daa
jon <janssen.nunag@gmail.com>
2023-03-19 21:22:13 -0400

    created controllers for getting all rounds by tournamentId and one round from a tournament, and creating a round in a tournament

--- controllers/tournamentController.js
+++ controllers/tournamentController.js
@@ -1,4 +1,5 @@
 const { Tournament } = require("../models/tournamentModel");
+const { Round } = require("../models/roundModel");
 
 module.exports.createTournament = async (req, res, next) => {
     let body = req.body;
@@ -30,5 +31,42 @@ module.exports.updateTournamentById = async (req, res, next) => {
 module.exports.deleteTournamentById = async (req, res, next) => {
     let id = req.params.id;
     await Tournament.findByIdAndDelete(id);
+    return next();
+}
+
+module.exports.getAllRoundsByTournamentId = async (req, res, next) => {
+    let id = req.params.id;
+    let tournament = await Tournament.findById(id, {rounds:1});
+    res.locals.tournament = tournament;
+    return next();
+}
+
+module.exports.getOneRoundFromTournamentById = async (req, res, next) => {
+    let tournamentId = req.params.id;
+    let roundId = req.params.round;
+
+    let tournament = await Tournament.findById(tournamentId, {rounds:1});
+    res.locals.round = tournament.rounds.id(roundId);
+
+    return next();
+}
+
+module.exports.updateRoundById = async (req, res, next) => {
+
+}
+
+module.exports.deleteRoundById = async (req, res, next) => {
+    
+}
+
+
+module.exports.createTournamentRound = async (req, res, next) => {
+    let id = req.params.id;
+    let body = req.body;
+    //let newRound = new Round(body);
+    let tournament = await Tournament.findById(id);
+    tournament.rounds.create(body);
+    await tournament.save();
+    
     return next();
 }
