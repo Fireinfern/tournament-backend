61436e362a9605490af3fdb7f1414f488ae26b8a
jon <janssen.nunag@gmail.com>
2023-03-21 18:13:35 -0400

    added route for getting all players by tournament id

--- routes/playerRoute.js
+++ routes/playerRoute.js
@@ -1,4 +1,8 @@
-const {} = require("../controllers/playerController");
+const { getAllPlayersByTournamentId } = require("../controllers/playerController");
 
 let router = require("express").Router();
 
+//get all players by tournamentId
+router.get('/:id/players', [getAllPlayersByTournamentId], async (req, res, next) => {
+    res.json(res.locals.players);
+});
