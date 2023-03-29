133d5ec026d56d26e7be26a9cc7e5953e06209f5
jon <janssen.nunag@gmail.com>
2023-03-29 17:15:59 -0400

    added code for authentication on index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const tournaments = require('./routes/tournamentRoute');
 const users = require('./routes/userRoute');
 
+//authentication modules
+let passport = require('passport');
+let passportJWT = require('passport-jwt');
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
 const port = process.env.PORT || 3000;
 const db = process.env.DB;
 
@@ -15,6 +21,33 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//create a user model for each user
+let userModel = require('./models/userModel');
+let User = userModel.User;
+
+//passport user configuration
+passport.use(User.createStrategy());
+
+//serialize and deserialize the user info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = DB.Secret; //need to change or define secret from db
+let strategy = new JWTStrategy(jwtOptions,(jwt_payload,done) => {
+  User.findById(jwt_payload.id).then(user => {
+    return done(null,user);
+  }).catch(err => {
+    return done(err, false);
+  });
+});
+passport.use(strategy);
 
 app.get('', (req, res, next) => {
     res.redirect('/v1/tournaments');
